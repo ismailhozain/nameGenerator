2413af1565a10b55c349867a022b3d83dfad5571
Ismail Hozain <ismailhozain@gmail.com>
2020-04-30 16:09:09 -0500

    Made CSV input.

--- src/Generator.java
+++ src/Generator.java
@@ -1,16 +1,31 @@
+import java.util.*;
+import java.io.*;
+
 
 public class Generator {
     private static String[] beginning;
     private static String[] middle;
     private static String[] end;
 
-    public Generator() {
+    public Generator() throws Exception {
         beginning = new String[] {"b", "d", "f", "g", "h", "k", "l", "m", "n", "s", "t", "v", "z"};
         middle = new String[] {"abt", "ikir", "mik", "zam", "nair"};
         end = new String[] {"ia", "ios", "idel", "ian", "iri", "i", "o", "ene", "erti", "on", "im", "adel", "ingo", "ica", "us"};
+        loadData();
+    }
+    private void loadData() throws Exception {
+        List<List<String>> records = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader("input.csv"))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] values = line.split(",");
+                records.add(Arrays.asList(values));
+                System.out.println(records);
+            }
+        }
+
 
     }
-
     public static String generateName() {
         int b = (int) (Math.random() * beginning.length);
         int m = (int) (Math.random() * middle.length);
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         Generator obj = new Generator();
         System.out.println(obj.toString());
     }
